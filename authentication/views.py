79d6e90cd0421f48339ce48625bb0abcea876f96
Antoliny <antoliny0919@naver.com>
2023-11-20 07:23:40 +0900

    authentication app views annotation update

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,22 @@
 import jwt
+from typing import Dict
 from django.conf import settings
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenViewBase
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from .serializers import CustomTokenObtainPairSerializer
 from users.serializers import UserSerializer
+from users.models import User
 
 # Create your views here.
 
-def get_token_for_user(user):
+def get_token_for_user(user: Dict[str, str]) -> Dict[str, str]:
+  '''
+  using user data for create access token & refresh token and return
+  '''
   refresh = RefreshToken.for_user(user)
   
   return {
@@ -18,12 +24,17 @@ def get_token_for_user(user):
     'access': str(refresh.access_token)
   }
 
-def get_expire(token_value):
+def get_expire(token_value: str) -> int:
+  '''
+  decode jwt token and get expires data
+  '''
   payload = jwt.decode(token_value, key=settings.SECRET_KEY, algorithms='HS256')
   return payload['exp']
 
-def login_success_response_jwt(user, token):
-  
+def login_success_response_jwt(user: User, token: Dict[str, str]) -> Response:
+  '''
+  serializing user data and jwt token data put dict together and return it
+  '''
   user = UserSerializer(user)
   token_data = {name: {'value': value, 'exp': get_expire(value)} for name, value in token.items()}
   data = {'token': token_data, 'user': user.data}
@@ -31,28 +42,26 @@ def login_success_response_jwt(user, token):
   return Response(data, status=status.HTTP_200_OK)
 
 
-class CustomTokenViewBase(generics.GenericAPIView):
-    permission_classes = ()
-    authentication_classes = ()
+class CustomTokenViewBase(TokenViewBase):
+  '''
+  change post method logic
+  TokenViewBase response only jwt token
+  CustomTokenViewBase response serializer user data and jwt token together
+  '''
 
-    serializer_class = None
-    _serializer_class = ""
+  def post(self, request: Request) -> Response:
+    serializer = self.get_serializer(data=request.data)
 
-    www_authenticate_realm = "api"
-
-    def post(self, request: Request) -> Response:
-        serializer = self.get_serializer(data=request.data)
-
-        try:
-            serializer.is_valid(raise_exception=True)
-        except TokenError as e:
-            raise InvalidToken(e.args[0])
-        
-        user = serializer.validated_data['user']
-        token = {name: value for name, value in serializer.validated_data.items() if name in ['access', 'refresh']}
-        response = login_success_response_jwt(user, token)
-        
-        return response
+    try:
+      serializer.is_valid(raise_exception=True)
+    except TokenError as e:
+      raise InvalidToken(e.args[0])
+    
+    user = serializer.validated_data['user']
+    token = {name: value for name, value in serializer.validated_data.items() if name in ['access', 'refresh']}
+    response = login_success_response_jwt(user, token)
+    
+    return response
 
 
 class CustomTokenObtainPairView(CustomTokenViewBase):
