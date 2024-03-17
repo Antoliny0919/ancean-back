5ef6fd95b3c183a8601e8e847d49df399103f745
Antoliny0919 <antoliny0919@gmail.com>
2024-03-18 06:48:08 +0900

    image upload view serailizer update

--- image/serializers.py
+++ image/serializers.py
@@ -1,4 +1,41 @@
+import os
+from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import serializers
 
 class ImageSerializer(serializers.Serializer):
-  pass
+  
+  file = serializers.ImageField()
+  id = serializers.IntegerField()
+  name = serializers.CharField()
+  
+  
+  def check_save_path(self, id, name):
+    user_post_store_path = os.path.join(getattr(settings, 'MEDIA_ROOT'), f'{name}/{id}')
+    
+    if not os.path.exists(user_post_store_path) or not id:
+      raise FileNotFoundError(f'{user_post_store_path} 이미지를 저장할 경로가 존재하지 않습니다.')
+    return user_post_store_path
+  
+  def validate(self, data):
+    """
+    """
+    id, name = data.get('id'), data.get('name')
+    user_post_store_path = self.check_save_path(id, name)
+    data.update({'user_post_store_path': user_post_store_path})
+    
+    return data
+  
+  
+  def create(self, validated_data):
+    
+    user_post_store_path, image_file = validated_data["user_post_store_path"], validated_data["file"]
+    save_path = os.path.join(user_post_store_path, image_file.name)
+    
+    with open(save_path, mode='wb') as file:
+      for chunk in image_file.chunks():
+        file.write(chunk)
+        
+    source_url = save_path.replace(str(getattr(settings, 'MAIN_DIR')), getattr(settings, 'SERVER_URI'))
+    
+    return {'success': 1, 'file': {'url': source_url, 'name': image_file.name}}
--- image/views.py
+++ image/views.py
@@ -1,13 +1,14 @@
 import os
 from django.conf import settings
-from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework import status, views
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
+from .serializers import ImageSerializer
+
 
 # Create your views here.
 
-class ImageView(APIView):
+class ImageView(views.APIView):
   
   permission_classes = [IsAdminUser]
   
@@ -17,17 +18,12 @@ class ImageView(APIView):
     path design --> '/ancean/media/{user_name}/{post_number(id)}/{file_name}'
     the id value of the post is required to save the image(it's mean image save only when post is created)
     '''
-    name = request.user.name
     data = request.data
-    id = data.get('id')
-    image_file = data.get('file')
-    user_image_store_path = f'{name}/{id}'
-    save_path = os.path.join(getattr(settings, 'MEDIA_ROOT'), user_image_store_path)
-    if not os.path.exists(save_path) or not id:
-      return Response({'detail': '이미지를 생성할 수 없습니다. 먼저 포스트를 생성해주세요(임시저장).'}, status=status.HTTP_400_BAD_REQUEST)
-    server = getattr(settings, 'SERVER_URI')
-    with open(os.path.join(save_path, image_file.name), mode='wb') as file:
-      for chunk in image_file.chunks():
-        file.write(chunk)
-    url = f'{server}/media/{name}/{id}/{image_file.name}'
-    return Response({"success": 1, "file": {"url": url, "name": image_file.name}}, status=status.HTTP_200_OK)
+    kwargs = {'file': data.get('file'), 'id': data.get('id'), 'name': request.user.name}
+    serializer = ImageSerializer(data=kwargs)
+    
+    if serializer.is_valid(raise_exception=True):
+      response = serializer.save()
+      return Response(response, status=status.HTTP_200_OK)
+  
+  
