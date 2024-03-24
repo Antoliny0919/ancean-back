3b62ed6d84f0b5c5fd1f6543b3be04fd8d3e6e84
Antoliny0919 <antoliny0919@gmail.com>
2024-03-24 20:14:31 +0900

    Post test patch method, none id, is_finish state, category model post_count field change observe

--- posts/tests/test_patch.py
+++ posts/tests/test_patch.py
@@ -1,25 +1,66 @@
 import json
+import pytest
 from rest_framework import status
 from category.models import Category
 
-def test_patch_post(base_client, category):
+
+def test_non_id_patch_post(base_client):
+  '''
+  post modification identifies the post to be corrected through the post id value of the request
+  this test when client did not send id case
+  '''
   created_post = base_client.user.post
   body = {'title': created_post.title, 'author': created_post.author.name, 'is_finish': False}
   error_response = base_client.patch(base_client.endpoint, json.dumps(body), content_type='application/Json')
+  
   assert error_response.status_code == status.HTTP_400_BAD_REQUEST
-  assert error_response.data.get('detail') == '포스트 id를 확인할 수 없습니다.'
-  
-  body['id'] = created_post.id
+  assert error_response.data == {'detail': '포스트 id를 확인할 수 없습니다.'}
 
+
+@pytest.mark.parametrize(
+  "is_finish, expected_response",
+  [
+    pytest.param(False, {'detail': '포스트가 임시저장되었습니다.'}),
+    pytest.param(True, {'redirect_path': '/posts/1/'})
+  ]
+)
+def test_is_finish_patch_post(base_client, is_finish, expected_response):
+  '''
+  test the change in the post modification success resposne according to the is_finish field value
+  '''
+  created_post = base_client.user.post
+  body = {
+    'id': created_post.id, 
+    'title': created_post.title, 
+    'author': created_post.author.name, 
+    'is_finish': is_finish
+  }
   response = base_client.patch(base_client.endpoint, json.dumps(body), content_type='application/Json')
-  
   assert response.status_code == status.HTTP_200_OK
-  # category = Category.objects.get(name=category.name)
-  # assert category.post_count == 0
+  assert response.data == expected_response
   
-  body['is_finish'] = True
-  response = base_client.patch(base_client.endpoint, json.dumps(body), content_type='application/Json') 
-  assert 0
-  # category =Category.objects.get(name=category.name)
-
-  # assert category.post_count == 1  
+  
+@pytest.mark.parametrize(
+  "is_finish, expected_post_count",
+  [
+    pytest.param(True, 1),
+    pytest.param(False, 0)
+  ]
+)
+def test_category_patch_post(category, base_client, is_finish, expected_post_count):
+  '''
+  when a post have a category
+  test the change in the post_count field value of the foreign key category
+  according to the is_finish field value
+  '''
+  created_post =  base_client.user.post
+  body = {
+    'id': created_post.id, 
+    'title': created_post.title, 
+    'author': created_post.author.name, 
+    'is_finish': is_finish, 
+    'category': category.name
+  }
+  base_client.patch(base_client.endpoint, json.dumps(body), content_type='application/Json')
+  category = Category.objects.get(name=category.name)
+  assert category.post_count == expected_post_count
