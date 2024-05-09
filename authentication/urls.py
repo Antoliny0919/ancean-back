9868c2957ef4270c49aa2b73f124c727a380d27a
Antoliny0919 <antoliny0919@gmail.com>
2024-05-09 16:59:16 +0900

    production env refresh token cookie have httponly option so update CustomTokenRefreshView use header cookie data

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-  TokenRefreshView,
-)
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, CustomTokenRefreshView
 
 
 urlpatterns = [
   path('token/', CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-  path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+  path('token/refresh/', CustomTokenRefreshView.as_view(), name="token_refresh"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,9 @@
+import json
 import os
 from django.conf import settings
 from rest_framework_simplejwt.views import (
-  TokenViewBase
+  TokenViewBase,
+  TokenRefreshView
 )
 from rest_framework_simplejwt.settings import api_settings
 
@@ -16,7 +18,7 @@ class CustomTokenViewBase(TokenViewBase):
     if env == 'local':
       cookie_conf = {'httponly': False, 'secure': False, 'samesite': 'lax'}
     else :
-      cookie_conf = {'httponly': True, 'secure': True, 'samesite': 'lax'}
+      cookie_conf = {'domain': 'ancean.net', 'httponly': True, 'secure': True, 'samesite': 'lax'}
 
     exp = getattr(settings, 'SIMPLE_JWT')['REFRESH_TOKEN_LIFETIME']
     exp_millisec = exp.total_seconds()
@@ -31,4 +33,25 @@ class CustomTokenViewBase(TokenViewBase):
     return super().finalize_response(request, response, *args, **kwargs)
 
 class CustomTokenObtainPairView(CustomTokenViewBase):
+
   _serializer_class = api_settings.TOKEN_OBTAIN_SERIALIZER
+
+class CustomTokenRefreshView(TokenRefreshView):
+  
+  def set_refresh_token(self):
+    '''
+    production server refresh token cookie have httponly attribute
+    so data extraction by parsing the cookie part from the request header
+    '''
+    
+    cookies_str = self.request.META["HTTP_COOKIE"]
+    if "refresh" in cookies_str:
+      cookies = {cookie.split("=")[0]: cookie.split("=")[1] for cookie in cookies_str.split("; ")}
+      self.request.data["refresh"] = json.dumps(cookies["refresh"]).replace("\"", '')
+
+  def post(self, request, *args, **kwargs):
+    env = os.environ.get('DJANGO_SETTINGS_MODULE').split('.')[-1]
+    if env != "local":
+      self.set_refresh_token()
+    
+    return super().post(request, *args, **kwargs)
